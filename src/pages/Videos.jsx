3d9c1c7dc7f32b9c2e6467bf86195d8cbaf0231e
Choi-HyunHo <x7788@naver.com>
2023-03-31 15:55:00 +0900

    style : tailwind css 적용 확인

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -4,7 +4,13 @@ import { useParams } from "react-router-dom";
 const Videos = () => {
 	const { keyword } = useParams();
 
-	return <span>Videos : {keyword}</span>;
+	return (
+		<>
+			<span className="text-3xl bg-gray-900 text-blue-500">
+				Videos : {keyword}
+			</span>
+		</>
+	);
 };
 
 export default Videos;
