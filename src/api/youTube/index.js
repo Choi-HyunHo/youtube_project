f20cab4c12e113da4aecdd044d6a8089af8029ea
최현호 <x7788@naver.com>
2023-04-03 19:54:44 +0900

    fix : key 유출로 인한 중지 및 재발급

--- src/api/youTube/index.js
+++ src/api/youTube/index.js
@@ -1,36 +1,36 @@
 import { instance } from "..";
 
 export const searchList = async (keyword) => {
-	const url = `search?part=snippet&maxResults=25&q=${keyword}&key=${process.env.REACT_APP_YOUTUBE_KEY}`;
-	const response = await instance.get(url).then((res) => {
-		return res.data.items;
-	});
-	return response;
+    const url = `search?part=snippet&maxResults=25&q=${keyword}&key=${process.env.REACT_APP_YOUTUBE_KEY}`;
+    const response = await instance.get(url).then((res) => {
+        return res.data.items;
+    });
+    return response;
 };
 
 export const popularList = async () => {
-	const url = `videos?part=snippet&chart=mostPopular&maxResults=25&key=${process.env.REACT_APP_YOUTUBE_KEY}`;
-	const response = await instance.get(url).then((res) => {
-		return res.data.items;
-	});
-	return response;
+    const url = `videos?part=snippet&chart=mostPopular&maxResults=25&key=${process.env.REACT_APP_YOUTUBE_KEY}`;
+    const response = await instance.get(url).then((res) => {
+        return res.data.items;
+    });
+    return response;
 };
 
 export const relatedList = async (id) => {
-	const url = `search?part=snippet&relatedToVideoId=${id}&type=video&maxResults=25&key=${process.env.REACT_APP_YOUTUBE_KEY}`;
-	const response = await instance.get(url).then((res) => {
-		return res.data.items;
-	});
+    const url = `search?part=snippet&relatedToVideoId=${id}&type=video&maxResults=25&key=${process.env.REACT_APP_YOUTUBE_KEY}`;
+    const response = await instance.get(url).then((res) => {
+        return res.data.items;
+    });
 
-	return response;
+    return response;
 };
 
 export const channelInfo = async (channelId) => {
-	const url = `channels?part=snippet&id=${channelId}&key=AIzaSyCf80JbHaMogFhGRWHdiwaays63R7eDWBY`;
-	const response = await instance.get(url).then((res) => {
-		console.log(res);
-		return res.data.items[0].snippet;
-	});
+    const url = `channels?part=snippet&id=${channelId}&key=${process.env.REACT_APP_YOUTUBE_KEY}`;
+    const response = await instance.get(url).then((res) => {
+        console.log(res);
+        return res.data.items[0].snippet;
+    });
 
-	return response;
+    return response;
 };
